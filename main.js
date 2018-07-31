e160fc05f88a27accd774d30dc76822c7c5e95d7
mataotao <1030557028@qq.com>
2018-07-31 14:48:38 +0800

    //在发送请求的过程中,每一毫秒问一下

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ myButton.addEventListener("click",(e)=>{
     let request = new XMLHttpRequest();
     request.open('GET','/xxx')//配置request.参数分别为方法和路径
     request.send();//发送请求
+    // console.log(request)
+    setInterval(()=>{//在发送请求的过程中,每一毫秒问一下
+        console.log(request.readyState);
+    },1)
 })
