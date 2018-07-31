550e6356b4589593f5df3babe1c5efcd167167af
mataotao <1030557028@qq.com>
2018-07-31 15:30:55 +0800

    request.readyState和request.status

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 let myButton = document.getElementById('myButton');
 myButton.addEventListener("click",(e)=>{
     let request = new XMLHttpRequest();
-    request.onreadystatechange = ()=>{//把监听函数往上写,这样就不会错过每一个readyState的变化
-        console.log(request.readyState);
+    request.onreadystatechange = ()=>{
+        if(request.readyState ===4){
+            console.log("请求和响应都完毕了");
+            if ( request.status>=200&&request.status<=400){
+                console.log('说明请求成功');
+                console.log(request.responseText);//打印响应的第四部分,字符串
+            }else if(request.status>=400){
+                console.log("响应失败");
+            }
+        } 
     }
     request.open('GET','/xxx')//配置request.参数分别为方法和路径
     request.send();//发送请求
