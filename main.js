9acff5ea249bee99577b3416719cf879ddc645ed
mataotao <1030557028@qq.com>
2018-07-31 14:51:57 +0800

    onreadystatechange测试

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let myButton = document.getElementById('myButton');
 myButton.addEventListener("click",(e)=>{
     let request = new XMLHttpRequest();
+    request.onreadystatechange = ()=>{//把监听函数往上写,这样就不会错过每一个readyState的变化
+        console.log(request.readyState);
+    }
     request.open('GET','/xxx')//配置request.参数分别为方法和路径
     request.send();//发送请求
     // console.log(request)
-    setInterval(()=>{//在发送请求的过程中,每一毫秒问一下
-        console.log(request.readyState);
-    },1)
 })
