37cade71f82a85a5ca2f126576f04c115867abf8
mataotao <1030557028@qq.com>
2018-07-31 12:03:22 +0800

    请求一个以xml格式解析的字符串

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let myButton = document.getElementById('myButton');
 myButton.addEventListener("click",(e)=>{
-    
+    let request = new XMLHttpRequest();
+    request.open('GET','/xxx')//配置request.参数分别为方法和路径
+    request.send();//发送请求
 })
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,19 +21,31 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/'){
+  if (path === '/') {
     response.statusCode = 200
-    let string = fs.readFileSync('./index.html') 
+    let string = fs.readFileSync('./index.html')
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path === '/main.js'){
+  } else if (path === '/main.js') {
     response.statusCode = 200
-    let string = fs.readFileSync('./main.js') 
+    let string = fs.readFileSync('./main.js')
     response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
     response.write(string)
     response.end()
-  }else{
+  } else if (path === '/xxx') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/xml;charset=utf-8')
+    response.write(`
+    <note>
+      <to>Tove</to>
+      <from>Jani</from>
+      <heading>Reminder</heading>
+      <body>Don't forget me this weekend!</body>
+    </note>
+    `)
+    response.end()
+  } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write('呜呜呜')
