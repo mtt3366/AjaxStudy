6f3bf8f747d65847c7b141da5992bab5ab406eb7
mataotao <1030557028@qq.com>
2018-08-05 20:05:42 +0800

    封装ajax1,添加失败时的回调函数,看看404时候的响应体

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
 let myButton = document.getElementById('myButton');
-myButton.addEventListener("click",(e)=>{
-    let request = new XMLHttpRequest();
-    request.open('POST','/xxx')//配置request
-    //设置第二部分
-    request.setRequestHeader("mataotao","123123xxx")
-    request.setRequestHeader('content-type','x-www-form-urlencoded')
-    request.send("a=1&b=2");//发送请求
 
+window.jQuery = ()=>{//假装有一个简易的jQuery,具体封装
+    let object1;
+    boject1.addClass = function(){};
+    boject1.show = function(){};
+    return object1;
+}
+
+window.jQuery.ajax = (method,path,body,successFn,failFn)=>{
+    let request = new XMLHttpRequest();
+    request.open(method,path)
+    request.send(body);
     request.onreadystatechange = ()=>{
         if(request.readyState ===4){
-            console.log("请求和响应都完毕了");
             if ( request.status>=200&&request.status<=400){
-                console.log('说明请求成功');
-
-                //*核心代码 */
-                //第一部分:
-                console.log("获取响应第一部分:")
-                console.log(request.status)//200
-                console.log(request.statusText)//ok
-
-                //第二部分:
-                console.log("获取响应第二部分:")
-                console.log(request.getResponseHeader('Content-Type'))
-                console.log(request.getAllResponseHeaders())
-
-                //第四部分:
-                console.log("获取响应第四部分:")
-                console.log(request.responseText)
-                 //*核心代码 */
-
-
-                let string = request.responseText;
-                //把符合json语法的字符串转化为js对应的值
-                let object2 = window.JSON.parse(string);
-                // console.log(object2)
-
+                successFn.call(undefined,request.responseText)//执行成功函数
             }else if(request.status>=400){
-                console.log("响应失败");
+                failFn.call(undefined,request)//执行失败函数
             }
-        } 
+        }
     }
+}
+
+window.$ = window.jQuery;
+myButton.addEventListener("click",(e)=>{
+    //使用ajax
+    $.ajax("post",
+           "/xxx",
+           "username=mtt&password=1",
+           function(result){
+                console.log('成功了,返回的响应体为:')
+                console.log(result);//打印request.responseText
+           },
+           function(result){
+                console.log(result);
+                console.log(result.status);//打印失败的状态码
+                console.log(result.responseText);//打印失败时返回的响应体
+                
+           }
+)
 })
