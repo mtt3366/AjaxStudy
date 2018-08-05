3ebfa0d77840a06e617481046bf43ac69ae60e3d
mataotao <1030557028@qq.com>
2018-08-05 20:51:54 +0800

    封装方法二:穿一个对象

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 let myButton = document.getElementById('myButton');
 
-window.jQuery = ()=>{//假装有一个简易的jQuery,具体封装
-    let object1;
+window.jQuery = ()=>{//假装有一个简易的jQuery
+    let object1 = {};
     boject1.addClass = function(){};
     boject1.show = function(){};
     return object1;
 }
 
-window.jQuery.ajax = (method,path,body,successFn,failFn)=>{
+window.jQuery.ajax = (options)=>{
+    //获取传进来的对象的value
+    let method = options.method;
+    let path = options.path;
+    let body = options.body;
+    let successFn = options.successFn;
+    let failFn = options.failFn;
+    let headers = options.headers;
+
     let request = new XMLHttpRequest();
-    request.open(method,path)
-    request.send(body);
+    request.open(method,path);//配置
+
+    for (const key in headers) {//遍历header,设置响应头
+        let value = headers[key];
+        request.setRequestHeader(key,value);
+    }
+    request.send(body);//发送,并配置响应体
+
     request.onreadystatechange = ()=>{
         if(request.readyState ===4){
             if ( request.status>=200&&request.status<=400){
-                successFn.call(undefined,request.responseText)//执行成功函数
+                successFn.call(undefined,request.responseText);//执行成功函数
             }else if(request.status>=400){
-                failFn.call(undefined,request)//执行失败函数
+                failFn.call(undefined,request);//执行失败函数
             }
         }
     }
@@ -25,18 +39,23 @@ window.jQuery.ajax = (method,path,body,successFn,failFn)=>{
 window.$ = window.jQuery;
 myButton.addEventListener("click",(e)=>{
     //使用ajax
-    $.ajax("post",
-           "/xxx",
-           "username=mtt&password=1",
-           function(result){
-                console.log('成功了,返回的响应体为:')
-                console.log(result);//打印request.responseText
-           },
-           function(result){
-                console.log(result);
-                console.log(result.status);//打印失败的状态码
-                console.log(result.responseText);//打印失败时返回的响应体
-                
-           }
+    $.ajax({
+        method:"post",
+        path:"/xxx",
+        body:"username=mtt&password=1",
+        headers:{
+            "content-type":'application/x-www-form-urlencoded',
+            "mataotao":18
+        },
+        successFn:function(result){//成功函数的回调
+            console.log('成功了,返回的响应体为:');
+            console.log(result);//打印request.responseTex
+        },
+        failFn:function(){
+            console.log(result);
+            console.log(result.status);//打印失败的状态码
+            console.log(result.responseText);//打印失败时返回的响应体
+        }
+    }
 )
 })
