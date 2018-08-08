20b8810989f9be1b9370d4a9b5783821b78d7272
mataotao <1030557028@qq.com>
2018-08-08 21:45:36 +0800

    再次对结果进行处理

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 let myButton = document.getElementById('myButton');
 
-function success(responseText){
+function success(responseText) {
     console.log("成功")
     console.log(responseText);//responseTex
 }
-function fail(request){
+function fail(request) {
     console.log("失败")
     console.log(request);
 }
-myButton.addEventListener("click",(e)=>{
-    //使用ajax
-    $.ajax({
-        method:"post",
-        url:"/xxx",
-        data:"username=mtt&password=1",
-        dataType:'json'//预期服务器返回的数据类型,如果不写,就是响应里设置的
-    }
-    ).then((responseText)=>{
-        console.log(responseText)
-        return responseText;//如果要对结果进行多次处理,就在这里return,第二次then就会得到这个return的数据
-    },()=>{
-        console.log("失败")
-    }).then(//开始第二次then
-        (上一次return的结果)=>{
-            console.log("第二次处理")
-            console.log(上一次return的结果)
-        },
-        ()=>{
-            //第二次处理失败结果
+
+if(myButton){//如果myButton存在,就添加事件
+    myButton.addEventListener("click", (e) => {
+        //使用ajax
+        $.ajax({
+            method: "post",
+            url: "/xxx",
+            data: "username=mtt&password=1",
+            dataType: 'json'//预期服务器返回的数据类型,如果不写,就是响应里设置的
         }
-    )
-})
+        ).then((responseText) => {
+            console.log(responseText)
+            return responseText;//如果要对结果进行多次处理,就在这里return,第二次then就会得到这个return的数据
+        }, () => {
+            console.log("失败")
+        }).then(//开始第二次then
+            (上一次return的结果) => {
+                console.log("第二次处理")
+                console.log(上一次return的结果)
+            },
+            () => {
+                //第二次处理失败结果
+                
+            }
+        )
+    })
+}
