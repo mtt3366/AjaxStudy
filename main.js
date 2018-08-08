125163e9cde04fea749be03b5e792e8f09aee90e
mataotao <1030557028@qq.com>
2018-08-08 12:34:07 +0800

    使用then 对结果进行多次处理

--- main.js
+++ main.js
@@ -16,11 +16,18 @@ myButton.addEventListener("click",(e)=>{
         data:"username=mtt&password=1",
         dataType:'json'//预期服务器返回的数据类型,如果不写,就是响应里设置的
     }
-    ).then(()=>{
-        console.log("成功")
-        console.log(responseText);
+    ).then((responseText)=>{
+        console.log(responseText)
+        return responseText;//如果要对结果进行多次处理,就在这里return,第二次then就会得到这个return的数据
     },()=>{
         console.log("失败")
-        console.log(request);
-    })//$.ajax()返回一个promise
+    }).then(//开始第二次then
+        (上一次return的结果)=>{
+            console.log("第二次处理")
+            console.log(上一次return的结果)
+        },
+        ()=>{
+            //第二次处理失败结果
+        }
+    )
 })
