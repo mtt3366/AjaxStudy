982acdc4f90fe037dee6656b05fe607f710591ef
mataotao <1030557028@qq.com>
2018-08-08 23:05:35 +0800

    使用Promise封装ajax()并使用

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
 let myButton = document.getElementById('myButton');
 
-function success(responseText) {
-    console.log("成功")
-    console.log(responseText);//responseTex
-}
-function fail(request) {
-    console.log("失败")
-    console.log(request);
+window.jQuery = ()=>{//假装有一个简易的jQuery
+    let object1 = {};
+    boject1.addClass = function(){};
+    boject1.show = function(){};
+    return object1;
 }
 
-if(myButton){//如果myButton存在,就添加事件
-    myButton.addEventListener("click", (e) => {
-        //使用ajax
-        $.ajax({
-            method: "post",
-            url: "/xxx",
-            data: "username=mtt&password=1",
-            dataType: 'json'//预期服务器返回的数据类型,如果不写,就是响应里设置的
+window.jQuery.ajax = ({method,path,body,headers})=>{//ES6语法
+   //进行Promise封装
+    return new Promise((resolve,reject)=>{//这句话是套路,记住
+        let request = new XMLHttpRequest();
+        request.open(method,path);//配置
+
+        for (const key in headers) {//遍历header,设置响应头
+            let value = headers[key];
+            request.setRequestHeader(key,value);
         }
-        ).then((responseText) => {
-            console.log(responseText)
-            return responseText;//如果要对结果进行多次处理,就在这里return,第二次then就会得到这个return的数据
-        }, () => {
-            console.log("失败")
-        }).then(//开始第二次then
-            (上一次return的结果) => {
-                console.log("第二次处理")
-                console.log(上一次return的结果)
-            },
-            () => {
-                //第二次处理失败结果
-                
+        request.send(body);//发送,并配置响应体
+
+        request.onreadystatechange = ()=>{
+            if(request.readyState ===4){
+                if ( request.status>=200&&request.status<=400){
+                    resolve.call(undefined,request.responseText);//执行成功函数
+                }else if(request.status>=400){
+                    reject.call(undefined,request);//执行失败函数
+                }
             }
-        )
+        }
     })
 }
+
+window.$ = window.jQuery;
+myButton.addEventListener("click",(e)=>{
+    //使用ajax
+    $.ajax({
+        method:"post",
+        path:"/xxx",
+        body:"username=mtt&password=1",
+        headers:{
+            "content-type":'application/x-www-form-urlencoded',
+            "mataotao":18
+        }
+    }).then(
+        (responseText)=>{console.log(responseText);},//成功就调用这个函数
+        (request)=>{console.log(request);}//失败就调用这个函数
+    )
+})
